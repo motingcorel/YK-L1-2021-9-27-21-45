4dc12472de416a166b7aac1e80e09eb35807ce42
motingcorel <artistfuture@gmail.com>
2021-09-27 21:45:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git weifuture https://github.com/hongweifuture/openwrt-packages' >>feeds.conf.default
+echo 'src-git pwdep https://github.com/hongweifuture/pwdep.git' >>feeds.conf.default
